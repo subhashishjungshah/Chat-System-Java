51c54d56a2982bae2eb41cd6bdb63e70e12b9931
subhashishshah <subhashish@ismt.edu.np>
2023-08-05 14:43:11 +0545

    server component ready for sending messages

--- src/Server.java
+++ src/Server.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -7,7 +8,6 @@ public class Server {
 
     ServerSocket server;
     Socket socket;
-
     BufferedReader br;
     PrintWriter out;
     public Server(){
@@ -16,15 +16,61 @@ public class Server {
             System.out.println("Server is Ready to accept Connection");
             System.out.println("Waiting...");
             socket = server.accept();
+
+//            Input stream Reader converts the byte code into ASCII character and load as a buffer. Buffer reader reads the data
+            br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream());
+            startReading();
+            startWriting();
         }
         catch (Exception e){
             e.printStackTrace();
         }
     }
+    public void startReading(){
+//        Let's make one thread for continously reading
+        Runnable r1 = () ->{
+            System.out.println("Reader Started...");
+            while(true){
+                try{
+                    String message = br.readLine();
+                    if(message.equals("quit")){
+                        System.out.println("Client terminated the chat");
+                        break;
+                    }
+                    System.out.println("Client:" + message);
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        };
+        new Thread(r1).start();
+    }
+    public void startWriting(){
+//        Let's make another thread for continuously writing
+        Runnable r2 = () ->{
+            System.out.println("Writer Started");
+            while (true){
+                try{
+                    BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
+                    String content = br1.readLine();
+                    out.println(content);
+                    out.flush();
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        };
+        new Thread(r2).start();
+    }
+
 
     public static void main(String[] args) {
 
         System.out.println("Server is staring inside your computer");
+        new Server();
 
         }
 }
